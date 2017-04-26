f27db28180494d3853cfffcd6b9595df8f958527
George Wu <uiharu@buaa.edu.cn>
2017-04-26 23:20:45 +0200

    Exporting to CSV files.

---
+++ StarlightDirector.Beatmap/Extensions/CompiledScoreExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using CsvHelper;
+using CsvHelper.Configuration;
+using StarlightDirector.Beatmap.Serialization;
+
+namespace StarlightDirector.Beatmap.Extensions {
+    public static class CompiledScoreExtensions {
+
+        public static string GetCsvString(this CompiledScore score) {
+            var sb = new StringBuilder();
+            using (var writer = new StringWriter(sb)) {
+                var config = new CsvConfiguration();
+                config.RegisterClassMap<ScoreCsvMap>();
+                config.HasHeaderRecord = true;
+                config.TrimFields = false;
+                using (var csv = new CsvWriter(writer, config)) {
+                    var newList = new List<CompiledNote>(score.Notes);
+                    csv.WriteRecords(newList);
+                }
+            }
+            sb.Replace("\r\n", "\n");
+            sb.Replace('\r', '\n');
+            return sb.ToString();
+        }
+
+    }
+}
