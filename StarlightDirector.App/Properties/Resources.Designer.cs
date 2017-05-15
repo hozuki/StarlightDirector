22df3c883b3934554479392d2b93101fb6b256fb
George Wu <uiharu@buaa.edu.cn>
2017-05-15 04:38:08 +0200

    Edit docs and related icon resources.

--- StarlightDirector.App/Properties/Resources.Designer.cs
+++ StarlightDirector.App/Properties/Resources.Designer.cs
@@ -263,6 +263,16 @@ namespace StarlightDirector.App.Properties {
         /// <summary>
         ///   查找 System.Drawing.Bitmap 类型的本地化资源。
         /// </summary>
+        internal static System.Drawing.Bitmap IconNoteHoldFlick {
+            get {
+                object obj = ResourceManager.GetObject("IconNoteHoldFlick", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   查找 System.Drawing.Bitmap 类型的本地化资源。
+        /// </summary>
         internal static System.Drawing.Bitmap IconNoteSlide {
             get {
                 object obj = ResourceManager.GetObject("IconNoteSlide", resourceCulture);
